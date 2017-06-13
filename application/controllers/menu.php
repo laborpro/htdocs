b54711786fb6d1e152393936004ce79324e7488b
filippVasin <vasin.filipp@yandex.ru>
2017-06-13 12:45:41 +0700

    второй коммит

--- application/controllers/menu.php
+++ application/controllers/menu.php
@@ -7,11 +7,10 @@
  */
 class Controller_menu extends Controller{
     // model, view и pointer - объявлены в родительском классе;
-
     public function exec_default(){
         // На уровне контроллера корректируем view меню в зависимости от состояния сессии или любых других параметров;
 
-        if(isset($_SESSION['name']) && $_SESSION['name'] != 'jjj') {
+        if(isset($_SESSION['name']) && $_SESSION['name'] != '') {
             $this->view = str_replace('<a href="/login"><div class="menu_button">Авторизация</div></a>', "", $this->view);
         }
     }
