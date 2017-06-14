d19bea6bffcf2cf2892e8043752e72891a710e91
filippVasin <vasin.filipp@yandex.ru>
2017-06-14 10:08:59 +0700

    laborpro

--- config.php
+++ config.php
@@ -1,10 +1,5 @@
 <?php
 // Параметры БД;
-//$sql_db_host = "127.0.0.1";
-//$sql_db = "laborpro";
-//$sql_db_user = "php";
-//$sql_db_password = "VIP1GAsA";
-
 $db_host = "127.0.0.1";
 $db_name = "laborpro";
 $db_user = "php";
